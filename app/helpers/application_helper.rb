0e3ec8179f70be196fe7c1acecac63dddf67f7b0
Boban Jovanoski <bobanj@gmail.com>
2012-03-07 22:08:04 +0100

    devise views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def flash_messages
+    msg = ''
+    flash.each do |key, value|
+      msg << <<-EOF
+        <div id="flash-#{key}" class="flash-message #{key}">#{value}</div>
+      EOF
+    end
+    msg.html_safe
+  end
+
 end
