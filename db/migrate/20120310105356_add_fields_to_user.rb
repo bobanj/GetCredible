60e2168d0d8078587498afe64b0860f6ec4b1a4d
Boban Jovanoski <bobanj@gmail.com>
2012-03-10 16:10:06 +0100

    remove fields from user

--- db/migrate/20120310105356_add_fields_to_user.rb
+++ db/migrate/20120310105356_add_fields_to_user.rb
@@ -1,16 +1,11 @@
 class AddFieldsToUser < ActiveRecord::Migration
   def change
     change_table :users do |t|
-      t.string :company_name
-      t.string :company_url
       t.string :first_name
       t.string :last_name
       t.string :job_title
-      t.string :street
-      t.string :zip
       t.string :city
       t.string :country
-      t.string :phone_number
       t.string :twitter_handle
       t.string :personal_url
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+usernames = ["test","voter"]
+password = "asdfasdf"
+usernames.each do |username|
+  email = "#{username}@test.com"
+  user = User.find_by_email email
+  unless user
+    puts "Setting up Test user with: #{email} / #{password}"
+    user = User.create :email => email, :password => password, :password_confirmation => password
+    user.save!
+  end
+end
+
