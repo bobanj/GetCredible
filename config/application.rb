2c9ebf20e8d995df339cf93f7a1f29b4a491907b
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-03-11 12:04:56 +0100

    Assets precompile task fix

--- config/application.rb
+++ config/application.rb
@@ -64,6 +64,7 @@ module GetCredible
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.precompile << /(^[^_]|\/[^_])[^\/]*/
+    # config.assets.precompile << /(^[^_]|\/[^_])[^\/]*/
+    config.assets.precompile << ['application.css', 'ie/application.css', 'application.js']
   end
 end
