7ee352534d0baa39c533c0ed28e7a13270bf7081
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-04-14 01:42:02 +0200

    Proper format for profile title

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,12 @@ module UsersHelper
   def disable_tagging(user)
     user == current_user || current_user.nil?
   end
+
+  def profile_title(user)
+    title = link_to(user.full_name, me_user_path(user))
+    if user.job_title.present?
+      title += ", #{user.job_title}"
+    end
+    title
+  end
 end
