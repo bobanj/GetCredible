51ed3079cdd794c5a7ced15a4aad8c8b3a74986e
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-04-13 20:44:39 +0200

    User can edit profile without entering passwords

---
+++ spec/integration/user_can_edit_their_account_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require_relative 'steps/app_steps'
+
+describe 'User', type: :request do
+
+  it "can edit their account" do
+    user = Factory :user, full_name: "Some Name"
+    sign_in_user(user)
+    click_link("Profile")
+    click_link("Edit profile")
+    fill_in("Full name", with: "Some other Name")
+    click_button("Save")
+    find_field("Full name").value.should == "Some other Name"
+  end
+end
+
