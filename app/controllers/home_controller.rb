6e821131b83858643cfb45f28af42c66a0822ec3
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-04-24 23:33:36 +0200

    Redirect to all activities page logged in user open landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
 
   def index
-    @activity_items = ActivityItem.order("created_at desc").
-      paginate(:per_page => 10, :page => 1)
+    if user_signed_in?
+      redirect_to activity_path('all')
+    else
+      @activity_items = ActivityItem.order("created_at desc").
+        paginate(:per_page => 10, :page => 1)
+    end
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,10 +2,19 @@ require 'spec_helper'
 
 describe HomeController do
 
-  describe "GET 'index'" do
+  describe "#index" do
     it "returns http success" do
-      get 'index'
-      response.should be_success
+      get :index
+
+      response.should render_template('index')
+    end
+
+    it "redirect to all activities if user is logged in" do
+      user = Factory(:user)
+      sign_in(user)
+
+      get :index
+      response.should redirect_to(activity_path('all'))
     end
   end
 
