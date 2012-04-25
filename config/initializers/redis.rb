485d550118c4aa3ddb2f4a37d161e82c032a2735
Boban Jovanoski <bobanj@gmail.com>
2012-04-25 23:33:02 +0200

    redis no namespace

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,4 +2,5 @@ require 'redis'
 require 'redis/objects'
 
 uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = $redis = Redis.current = Redis::Namespace.new('give_brand', :redis => Redis.new(:host => uri.host, :port => uri.port, :password => uri.password))
+#REDIS = $redis = Redis.current = Redis::Namespace.new('give_brand', :redis => Redis.new(:host => uri.host, :port => uri.port, :password => uri.password))
+REDIS = $redis = Redis.current = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
