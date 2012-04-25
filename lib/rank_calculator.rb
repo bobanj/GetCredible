6e201aa14f435745552abf8c328d6389724118f4
Boban Jovanoski <bobanj@gmail.com>
2012-04-25 21:27:09 +0200

    rank kalculator score to 100

--- lib/rank_calculator.rb
+++ lib/rank_calculator.rb
@@ -1,13 +1,14 @@
-class Float
-  # 0.666666666 -> 66.7
-  def to_percentage
-    100 * (self * (10 ** 3)).round / (10 ** 3).to_f
-  end
-end
+#class Float
+#  # 0.666666666 -> 66.7
+#  def to_percentage
+#    100 * (self * (10 ** 3)).round / (10 ** 3).to_f
+#  end
+#end
 
 require 'redis/sorted_set'
+
 class RankCalculator
-  attr_accessor :probability, :tolerance
+  attr_accessor :probability, :tolerance, :score_max
 
   def initialize
     # The bigger the number, less probability
@@ -16,6 +17,7 @@ class RankCalculator
     # the smaller the number, the more exact the
     # result will be but more CPU cycles will be needed
     @tolerance = 0.0001
+    @score_max = 100
   end
 
   def calculate
@@ -23,15 +25,16 @@ class RankCalculator
     #ActiveRecord::Base.connection.execute('DELETE FROM "user_tag_results"')
     Tag.all.each do |tag|
       total_user_tags = tag.user_tags.count
-      #scale_max = probability * total_user_tags + (1 - probability)
-      #base = scale_max ** 0.01
-      #scale_range = []
-      #(1..100).to_a.inject(scale_max) { |result, scale_element|
-      #  scale_range << Range.new(result / base, result)
-      #  result / base
-      #}
-      #scale_range << Range.new(0, scale_range.last.min)
-      #scale_range.reverse!
+      scale_max = probability * total_user_tags + (1 - probability)
+      base = scale_max ** 0.01
+
+      scale_range = []
+      (1..score_max).to_a.inject(scale_max) { |result, scale_element|
+        scale_range << Range.new(result / base, result)
+        result / base
+      }
+      scale_range << Range.new(0, scale_range.last.min)
+      scale_range.reverse!
 
       rankable_graph = RankableGraph.new
       tag_scores = Redis::SortedSet.new("tag:#{tag.id}:scores")
@@ -67,23 +70,24 @@ class RankCalculator
 
         weight = rank.to_f * total_user_tags.to_f * (incoming.to_f / outgoing.to_f)
         weight = 1.to_f + weight if weight < 1
-        tag_scores[user_tag_id] = weight unless weight.infinite?
-
-        #if weight > scale_max
-        #  # Change if changing scale
-        #  user_tag.weight = 100
-        #  puts "user_tag.id:#{user_tag.id}  rank: #{rank}  incoming: #{incoming}   outgoing: #{outgoing}    weight:#{weight}"
-        #  #UserTagResult.create tag_id: user_tag.tag_id, user_id: user_tag.user_id, user_tag_id: user_tag.id, rank: rank, incoming: incoming, outgoing: outgoing, weight: weight, weight_index: index
-        #else
-        #  scale_range.each_with_index do |range, index|
-        #    if range.cover?(weight)
-        #      user_tag.weight = index
-        #      puts "user_tag.id:#{user_tag.id}  rank: #{rank}  incoming: #{incoming}   outgoing: #{outgoing}    weight:#{weight}  index: #{index}"
-        #      #UserTagResult.create tag_id: user_tag.tag_id, user_id: user_tag.user_id, user_tag_id: user_tag.id, rank: rank, incoming: incoming, outgoing: outgoing, weight: weight, weight_index: index
-        #      break
-        #    end
-        #  end
-        #end
+        if weight.infinite?
+          raise "Infinite Weight for #{user_tag_id}"
+          return false
+        end
+        if weight > scale_max
+          tag_scores[user_tag_id] = score_max
+          #puts "user_tag.id:#{user_tag.id}  rank: #{rank}  incoming: #{incoming}   outgoing: #{outgoing}    weight:#{weight}"
+          #UserTagResult.create tag_id: user_tag.tag_id, user_id: user_tag.user_id, user_tag_id: user_tag.id, rank: rank, incoming: incoming, outgoing: outgoing, weight: weight, weight_index: index
+        else
+          scale_range.each_with_index do |range, index|
+            if range.cover?(weight)
+              tag_scores[user_tag_id] = weight
+              #puts "user_tag.id:#{user_tag.id}  rank: #{rank}  incoming: #{incoming}   outgoing: #{outgoing}    weight:#{weight}  index: #{index}"
+              #UserTagResult.create tag_id: user_tag.tag_id, user_id: user_tag.user_id, user_tag_id: user_tag.id, rank: rank, incoming: incoming, outgoing: outgoing, weight: weight, weight_index: index
+              break
+            end
+          end
+        end
       end
 
     end
