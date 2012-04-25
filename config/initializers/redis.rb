981c940bdd45211aa7b15a4df2e170adddddd481
Boban Jovanoski <bobanj@gmail.com>
2012-04-25 22:19:55 +0200

    redis connection fix

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,4 +2,4 @@ require 'redis'
 require 'redis/objects'
 
 uri = URI.parse(Settings.redis.url)
-Redis.current = Redis::Namespace.new(Settings.redis.namespace, :redis => Redis.new(:host => uri.host, :port => uri.port))
+Redis.current = Redis::Namespace.new(Settings.redis.namespace, :redis => Redis.new(:host => uri.host, :port => uri.port, :password => uri.password))
