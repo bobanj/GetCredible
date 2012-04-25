d6e1b5fb340936ca02f3107e5820987985a5f03a
Boban Jovanoski <bobanj@gmail.com>
2012-04-25 23:11:07 +0200

    hardcode redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,10 @@
 require 'redis'
 require 'redis/objects'
 
-uri = URI.parse(Settings.redis.url)
-Redis.current = Redis::Namespace.new(Settings.redis.namespace, :redis => Redis.new(:host => uri.host, :port => uri.port, :password => uri.password))
+if(Rails.env.production?)
+  redis_url = 'redis://redistogo:9883ab2d8a51f29a335f3256ac4a9c1d@drum.redistogo.com:9223/'
+else
+  redis_url = 'redis://127.0.0.1:6379'
+end
+uri = URI.parse(redis_url)
+Redis.current = Redis::Namespace.new('give_brand', :redis => Redis.new(:host => uri.host, :port => uri.port, :password => uri.password))
