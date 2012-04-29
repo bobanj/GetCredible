f62117c79a60d84c730bd3184aeb784e60503070
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-04-29 17:21:32 +0200

    Changed subjects for all emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,20 +3,20 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-    mail(to: user.email, subject: "[GiveBrand] Welcome to GiveBrand!")
+    mail(to: user.email, subject: "Welcome to GiveBrand!")
   end
 
   def tag_email(tagger, tagged, tag_names)
     @tagger    = tagger
     @tagged    = tagged
     @tag_names = tag_names
-    mail(to: tagged.email, subject: "[GiveBrand] You have been tagged!")
+    mail(to: tagged.email, subject: "You have been tagged!")
   end
 
   def vote_email(voter, voted, tag_name)
     @voter    = voter
     @voted    = voted
     @tag_name = tag_name
-    mail(to: voted.email, subject: "[GiveBrand] You received a vote!")
+    mail(to: voted.email, subject: "You received a vote!")
   end
 end
