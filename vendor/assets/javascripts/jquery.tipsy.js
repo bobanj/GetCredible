815d0d7ee8b47b86090ac245682467e78e3fbf63
Dime Pashoski <dime@pashoski.com>
2012-05-04 22:25:03 +0200

    tipsy improved, tipsy overlay test, screenshot 3 replaced

--- vendor/assets/javascripts/jquery.tipsy.js
+++ vendor/assets/javascripts/jquery.tipsy.js
@@ -103,7 +103,7 @@
         
         tip: function() {
             if (!this.$tip) {
-                this.$tip = $('<div class="tipsy"></div>').html('<div class="tipsy-arrow"></div><div class="tipsy-inner"></div>');
+                this.$tip = $('<div class="tipsy"></div>').html('<div class="tipsy-arrow"></div><div class="tipsy-inner"></div><!--<div class="tag-overlay"></div>-->');
             }
             return this.$tip;
         },
