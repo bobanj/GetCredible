957c3b99d3cb8218e7c3bd7cba959909817809d6
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-05-08 20:29:24 +0200

    User can have dot in the name

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,7 @@ class User < ActiveRecord::Base
   has_many :voters, :through => :user_tags, :uniq => true
 
   # Validations
-  validates :full_name, :presence => true, :format => {:with => /^[\w\s-]*$/}
+  validates :full_name, :presence => true
   validates :personal_url, :url_format => true, :allow_blank => true
 
   # Callbacks
