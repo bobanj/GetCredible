cf34fd64f698d7a4a57b06073558010e0be143df
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-05-08 23:28:11 +0200

    Changed host for staging environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -66,16 +66,16 @@ GetCredible::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Devise Mailer
-  config.action_mailer.default_url_options = { :host => 'givebrand.to' }
+  config.action_mailer.default_url_options = { :host => 'givebrand-staging.herokuapp.com' }
 end
 
 ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com'
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com'
 }
 ActionMailer::Base.delivery_method = :smtp
 
