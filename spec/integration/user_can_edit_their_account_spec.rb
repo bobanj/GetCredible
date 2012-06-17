6b66c4fe040dada279dad9a125171bb48a347c94
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-06-17 15:40:32 +0200

    New sidebars

--- spec/integration/user_can_edit_their_account_spec.rb
+++ spec/integration/user_can_edit_their_account_spec.rb
@@ -7,13 +7,13 @@ describe 'User', type: :request do
     user = FactoryGirl.create(:user, full_name: "Some Name")
     sign_in_user(user)
     click_link("Profile")
-    click_link("Edit profile")
+    click_link("Edit")
     fill_in("Full name", with: "Some other Name")
     click_button("Save")
     page.should have_content("You have updated your profile successfully.")
 
     click_link("Profile")
-    click_link("Edit profile")
+    click_link("Edit")
     find_field("Full name").value.should == "Some other Name"
   end
 end
