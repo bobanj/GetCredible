0b1c5466ea834fb9f48010e0d503a971480ceb52
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-06-19 18:50:04 +0200

    Removed unused controller code for activities

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,7 @@
 class HomeController < ApplicationController
-  include UserTagsHelper
 
   def index
-    if user_signed_in?
-      redirect_to activity_path('all')
-    else
-      @activity_items = ActivityItem.order("created_at desc").
-        paginate(:per_page => 10, :page => 1)
-      preload_associations(@activity_items)
-    end
+    redirect_to activity_path('all') if user_signed_in?
   end
 
   def privacy
