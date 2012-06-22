77c8083e3cfc67ed1e2efe14acd636b78eafba05
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-06-22 20:29:40 +0200

    Fixed carrierwave Amazon S3 for staging env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,5 @@
 CarrierWave.configure do |config|
-  if Rails.env.production?
+  if Rails.env.production? || Rails.env.staging?
     config.fog_credentials = {
         # Configuration for Amazon S3 should be made available through an Environment variable.
         # For local installations, export the env variable through the shell OR
