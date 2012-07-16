2eb9431adf36f9eba855db84621085c656c7bdf0
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-07-16 23:56:08 +0200

    Resque connect to correct redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -6,3 +6,4 @@ uri = URI.parse(ENV["REDISTOGO_URL"] || 'redis://127.0.0.1:6379')
 REDIS = $redis = Redis.current = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 Soulmate::MIN_COMPLETE = 2
 Soulmate.redis = ENV["REDISTOGO_URL"]
+Resque.redis = REDIS
