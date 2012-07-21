67c600a64d79eafb89c0ea8ad13b0358ff46d4cc
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-07-21 23:24:06 +0200

    User can delete their own endorsements

--- spec/integration/user_can_delete_their_own_endorsements_spec.rb
+++ spec/integration/user_can_delete_their_own_endorsements_spec.rb
@@ -2,7 +2,23 @@ require 'spec_helper'
 
 describe 'User', type: :request do
 
-  it "can delete their own endorsements" do
-    pending
+  it "can delete their own endorsements", js: true do
+    endorser = FactoryGirl.create(:user, full_name: "Endorser")
+    user = FactoryGirl.create(:user, full_name: "Pink Panter")
+    tag = FactoryGirl.create(:tag)
+    user_tag = FactoryGirl.create(:user_tag, user: user, tagger: endorser, tag: tag)
+    endorsement = FactoryGirl.create(:endorsement, endorser: endorser,
+                     user_tag: user_tag, description: "This is my endorsement")
+
+    sign_in_user(user)
+
+    visit me_user_path(user)
+
+    page.should have_content("This is my endorsement")
+
+    within(".js_endorsement") do
+      click_link("X")
+    end
+    page.should_not have_content("This is my endorsement")
   end
 end
