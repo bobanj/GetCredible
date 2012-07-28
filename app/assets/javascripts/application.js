08ec16c3cfffd5c23d19927b2c4a189e341d2d1c
Boban Jovanoski <bobanj@gmail.com>
2012-07-28 18:40:43 +0200

    landing page video in modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -57,23 +57,6 @@ function onYouTubePlayerAPIReady(){
       }
     }
   });
-  landingVideoApi = new YT.Player('landing-video', {
-    playerVars:{
-      autoplay:0,
-      enablejsapi:1,
-      origin:document.location.host
-    },
-    origin:document.location.host,
-    height:315,
-    width:560,
-    videoId:$.getCredible.guideVideoId,
-    events:{
-      'onReady':function (e){
-        // Store the player in the API
-        landingVideoApi = e.target;
-      }
-    }
-  });
 }
 
 $(function (){
@@ -1116,19 +1099,73 @@ $(function (){
   }
 
   $.getCredible.landingPageVideo = function () {
-    $('.video').click(function () {
-      $("#landing-video").show('fast',function(){
-        if ($.isFunction(landingVideoApi.playVideo)){
-          landingVideoApi.playVideo();
-        } else {
-          var videoTmp = setTimeout(function () {
-            if ($.isFunction(landingVideoApi.playVideo)){
-              landingVideoApi.playVideo();
-            }
-          }, 800)
+        $('.video').qtip({
+      content:{
+        text: $('<div />', { id: 'landing-video' }),
+        title:{
+          text:' ',
+          button:true
         }
-      });
+      },
+      position:{
+        my:'center', // ...at the center of the viewport
+        at:'center',
+        target:$(window)
+      },
+      show:{
+        ready:false,
+        solo:true, // ...and hide all other tooltips...
+        event:'click',
+        modal:{
+          on:true,
+          blur:false,
+          escape:true
+        },
+        effect: function() {
+          var style = this[0].style;
+          style.display = 'none';
+          setTimeout(function() { style.display = 'block'; }, 1);
+        }
+      },
+      hide: false,
+      events:{
+        render: function (event, api){
+          landingVideoApi = new YT.Player('landing-video', {
+            playerVars:{
+              autoplay:1,
+              enablejsapi:1,
+              origin:document.location.host
+            },
+            origin:document.location.host,
+            height:315,
+            width:560,
+            videoId:$.getCredible.guideVideoId,
+            events:{
+              'onReady':function (e){
+                // Store the player in the API
+                api.player = e.target;
+              }
+            }
+          });
+        },
+        show: function(event, api){
+          // Pause the video when tooltip is hidden
+          if(api.player && $.isFunction(api.player.playVideo)) {
+            api.player.playVideo();
+          }
+        },
+        hide: function(event, api){
+          // Pause the video when tooltip is hidden
+          if(api.player && $.isFunction(api.player.stopVideo)) {
+            api.player.stopVideo();
+          }
+        }
+      },
+      style:{
+        classes:'ui-tooltip-light ui-tooltip-landing-video'
+      }
     });
+
   };
 
   $.getCredible.showFlashMessages();
