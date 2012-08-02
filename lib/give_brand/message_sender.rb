82bc2200320083b52e8d1ac5539884779ddd6695
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-08-03 00:24:12 +0200

    New social invitation messages

--- lib/give_brand/message_sender.rb
+++ lib/give_brand/message_sender.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 module GiveBrand
   class MessageSender
     class FacebookChatAccessDenied < StandardError; end
@@ -17,9 +19,9 @@ module GiveBrand
     def send_message
       case provider
       when 'twitter'
-        client.direct_message_create(screen_name, message_body_short)
+        client.direct_message_create(screen_name, twitter_message)
       when 'linkedin'
-        client.send_message(message_subject, message_body, [receiver_uid])
+        client.send_message(message_subject, linkedin_message, [receiver_uid])
       when 'facebook'
         send_facebook_message
       end
@@ -35,23 +37,15 @@ module GiveBrand
           :invitation_token => receiver.invitation_token)
     end
 
-    def message_body
-      "I've tagged you with \"#{tag_names.join(', ')}\" on GiveBrand! Start building your profile here: #{invitation_url}"
-    end
-
-    def message_body_short
-      "I've tagged you with \"#{tag_names.first}\" on GiveBrand! Build your profile: #{invitation_url}"
-    end
-
     def message_subject
-      "Come build your profile at GiveBrand!"
+      "#{invitation_message.inviter.name} has invited you to create a GiveBrand profile"
     end
 
     def send_facebook_message
       facebook_auth    = inviter.facebook_authentication
       receiver_chat_id   = "-#{receiver_uid}@chat.facebook.com"
       sender_chat_id = "-#{facebook_auth.uid}@chat.facebook.com"
-      jabber_message   = Jabber::Message.new(receiver_chat_id, message_body)
+      jabber_message   = Jabber::Message.new(receiver_chat_id, facebook_message)
       jabber_message.subject = message_subject
 
       client = Jabber::Client.new(Jabber::JID.new(sender_chat_id))
@@ -63,6 +57,26 @@ module GiveBrand
       client.close
     rescue RuntimeError
       raise FacebookChatAccessDenied, "No access to Facebook Chat"
+    end
+
+    def linkedin_message
+      "Dear #{receiver.name}:
+      I'm building my personal brand on http://givebrand.com, a website that lets jobseekers and other professionals invite their friends and colleagues to weigh in on their best attributes.\n
+      My profile is an easy-to-understand visual snapshot of my skills and traits that I'll be able to share with clients, hiring managers and other professional contacts.\n
+      I thought you might like to sign up for your own profile: #{invitation_url}. To get you started, I've tagged you with \"#{tag_names.join(', ')}\".\n
+      See you on http://givebrand.com!"
+    end
+
+    def facebook_message
+      "I'm building my personal brand on http://givebrand.com, a website that puts a social spin on my résumé by letting me invite my friends and colleagues to weigh in on my best attributes.\n
+      They're helping me create an easy-to-understand visual snapshot of my skills and traits that I'll be able to share with clients, hiring managers and other professional contacts.\n
+      I thought you might like to sign up for your own profile: #{invitation_url}. I've tagged you with \"#{tag_names.join(', ')}\" to get you started. Please join me!\n
+      See you there."
+    end
+
+    def twitter_message
+      tag = tag_names.first.to_s.first(22)
+      "Put a social twist on your résumé on @GiveBrand! I tagged you w/ \"#{tag}\" to get you started — try it! #{invitation_url}"
     end
   end
 end
