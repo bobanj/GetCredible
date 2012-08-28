5ee28eba03f5ec715f8571568a66ab33af9c4d93
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-08-28 12:07:15 +0200

    Remove PaginationNoFollow

--- lib/pagination_no_follow.rb
+++
@@ -1,7 +0,0 @@
-require 'will_paginate/view_helpers/action_view'
-
-class PaginationNoFollow < WillPaginate::ActionView::LinkRenderer
-  def rel_value(page)
-    [super, 'nofollow'].compact.join(' ')
-  end
-end
