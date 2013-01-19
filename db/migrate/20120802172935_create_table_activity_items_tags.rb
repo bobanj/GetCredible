0b6fbd553ad4c7b7d30a174a36eb410714268050
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2013-01-19 15:28:07 +0100

    Remove datafixes from migrations

--- db/migrate/20120802172935_create_table_activity_items_tags.rb
+++ db/migrate/20120802172935_create_table_activity_items_tags.rb
@@ -7,22 +7,6 @@ class CreateTableActivityItemsTags < ActiveRecord::Migration
 
     add_index :activity_items_tags, [:activity_item_id, :tag_id]
     add_index :activity_items_tags, [:tag_id, :activity_item_id]
-
-    puts "remove orphan activity items"
-    ActivityItem.all.select{|a| a.target.blank? || a.item.blank?}.map(&:destroy)
-
-    puts "linking activity items with tags"
-    ActivityItem.all.each do |activity_item|
-      print "."
-      if activity_item.item_type == 'Vote'
-        activity_item.tags = [activity_item.item.user_tag.tag]
-      elsif activity_item.item_type == 'UserTag'
-        activity_item.tags = [activity_item.item.tag]
-      else #if activity_item.item_type == 'Endorsement'
-        activity_item.tags = [activity_item.item.user_tag.tag]
-      end
-      activity_item.save!
-    end
   end
 
   def down
